f1f45abd6c9844cb33f682a32036b899b1fce417
Jared Rummler <jared.rummler@gmail.com>
2016-04-23 05:23:11 -0700

    remove script

--- rename.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-find ./dafont ./fontsquirrel ./googlefonts -type f -name info.json -mindepth 3 -print | while read file
-do
-	sed -i 's|"varient":|"variant":|' $file
-	echo $file
-done
